53710d7cf41a33f541b7d1051b332fe4ec51f447
Goteusz Maszyk <45947374+goteusz-maszyk@users.noreply.github.com>
2020-04-21 18:40:45 +0200

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,33 @@
+function calculate() {
+  let output
+  let num1 = Number(document.forms["calcForm"]["num1"].value);
+  let num2 = Number(document.forms["calcForm"]["num2"].value);
+  let dzialanie = document.forms["calcForm"]["dzialanie"].value;
+  if(dzialanie == '+') {
+    output = num1 + num2
+  } else if(dzialanie == '-') {
+    output = num1 - num2
+  } else if(dzialanie == '/') {
+    output = num1 / num2
+  } else if(dzialanie == '*') {
+    output = num1 * num2
+  } else if(dzialanie == 'sm') {
+    output = 0
+    alert('Wyliczasz średnią arytmetyczną')
+    liczby = []
+    while(true) {
+      if (confirm('Czy chcesz więcej liczb?')) {
+        liczby.push(Number(prompt('Podaj liczbę')))
+      } else {
+        for(i=0; i < liczby.length; i++) {
+          output += liczby[i]
+        }
+        output = output / liczby.length
+        break
+      }
+    }
+  }
+  if(isNaN(output) || output == undefined) { output = 0 }
+  $('#output').text('Wynik: ' + output);
+  return false
+}
